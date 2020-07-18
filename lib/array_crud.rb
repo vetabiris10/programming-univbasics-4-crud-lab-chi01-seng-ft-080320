3fd33978dc7bcdf440c6118da157e79d5f88a1c1
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-18 05:01:07 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,10 +17,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  fruit = ["apple", "pear","orange", "strabery"]
   fruit.pop
 end
 
 def remove_element_from_start_of_array(array)
+  fruit = ["apple", "pear","orange", "strabery"]
   fruit.shift
 end
 
